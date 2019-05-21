fa4f09f85d8bd75d052436185a581d1e19d74c02
omarradwan6 <omarradwan91@gmail.com>
2019-05-21 22:31:56 +0200

    First commit

---
+++ colors-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Palette from './Palette'
+import seedColors from './seedColors'
+
+
+function App() {
+  return (
+  <Palette color={seedColors[1]}/>
+  );
+}
+
+export default App;
---
+++ colors-app/src/Palette.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Palette extends React.Component {
+  
+  render(){
+    return (
+        <div>pppp</div>
+      );
+  }
+
+}
+
+export default Palette;
